c2e6d9c295cec412f97cdedb223c690b79c3309a
touraye <touraye07@gmail.com>
2021-01-27 10:11:55 +0000

    Added student info

---
+++ Student-info/Address.java
@@ -0,0 +1,22 @@
+// Q4
+public class Address {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String stuName = "Ebrima Faal";
+		String Address = "Jam City";
+		String email = "somebody@yahoo.com";
+		int phoneNumber = 0210101;
+		int gpa = 3.9;
+		String program = "Computer Science";
+		
+		System.out.println("Name: " + StuName);
+		System.out.println("Address: " + Address);
+		System.out.println("Phone: " + phoneNumber);
+		System.out.println("email: " + email);
+		System.out.println("gpa: " + gpa);
+		System.out.println("program: " + program);
+	}
+
+}
